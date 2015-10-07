f8a01b08dd5fc52c6f28ff6e5d138487f96ef7ca
Andrew Copp <andrew@hinge.co>
2015-10-07 16:39:10 -0400

    Moving to VIPER

---
+++ Draggable/DraggableCollectionPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  DraggableCollectionPresenter.swift
+//  Draggable
+//
+//  Created by Andrew Copp on 10/7/15.
+//  Copyright © 2015 Andrew Copp. All rights reserved.
+//
+
+import UIKit
+
+class DraggableCollectionPresenter: NSObject {
+
+}
